e6f8a92013e89c419bceb464daa42eb0931f714d
chenxuan <chenxa@maxchaintech.com>
2021-06-23 16:59:20 +0800

    token

---
+++ token/identity.go
@@ -0,0 +1,23 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package token
+
+// Identity refers to the creator of a tx;
+type Identity interface {
+	Serialize() ([]byte, error)
+}
+
+// SigningIdentity defines the functions necessary to sign an
+// array of bytes; it is needed to sign the commands transmitted to
+// the prover peer service.
+type SigningIdentity interface {
+	Identity //extends Identity
+
+	Sign(msg []byte) ([]byte, error)
+
+	GetPublicVersion() Identity
+}
